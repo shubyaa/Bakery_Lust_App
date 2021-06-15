b8d56d034cc76283872ce0e4faf3ab29d56a4027
Shubhamm Pednekar <67822091+shubyaa@users.noreply.github.com>
2021-06-15 19:18:24 +0530

    Added simple SignUp intent and made changes in UI of LoginActivity.java

--- app/src/main/java/com/example/bakery_lust/LoginActivity.java
+++ app/src/main/java/com/example/bakery_lust/LoginActivity.java
@@ -2,13 +2,36 @@ package com.example.bakery_lust;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.SimpleAdapter;
 
 public class LoginActivity extends AppCompatActivity {
+    Button login, signup, google, facebook;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        login = findViewById(R.id.login);
+        signup = findViewById(R.id.signup);
+        google = findViewById(R.id.google);
+        facebook = findViewById(R.id.facebook);
+
+        Signup();
+
+    }
+
+    private void Signup(){
+        signup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent in = new Intent(getApplicationContext(), SignUpActivity.class);
+                startActivity(in);
+            }
+        });
     }
 }
